9c963b56976403765654cf50bf9f62c43a0a0156
Daniel Song <81377675+djsong15@users.noreply.github.com>
2025-11-09 15:48:12 -0800

    Reformat

--- Exercises/Exercise17/PrintingShapes.java
+++ Exercises/Exercise17/PrintingShapes.java
@@ -1,3 +1,4 @@
+
 // TODO: Don't forget to import the Scanner class for Part 5
 import java.util.Scanner;
 
@@ -5,29 +6,29 @@ import java.util.Scanner;
  * The PrintHelper class has the methods for printing various shapes
  */
 class PrintHelper {
-	// Your methods to print various shapes goes here
-	public void printStars(int width) {
-		System.out.println("Stars:");
-		int whichCol = 0;
-		while (whichCol < width) {
-			System.out.print("*");
-			++whichCol;
-		}
-		// Print just a new-line character
-		System.out.println("");
-	}
+    // Your methods to print various shapes goes here
+    public void printStars(int width) {
+        System.out.println("Stars:");
+        int whichCol = 0;
+        while (whichCol < width) {
+            System.out.print("*");
+            ++whichCol;
+        }
+        // Print just a new-line character
+        System.out.println("");
+    }
 
-	public void printRectangle(int width, int height) {
-		System.out.println("Rectangle:");
-		// TODO: Change this method's incomplete code to use nested for-loops
-		for (int i = 0; i < height; i++) {
+    public void printRectangle(int width, int height) {
+        System.out.println("Rectangle:");
+        // TODO: Change this method's incomplete code to use nested for-loops
+        for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-	}
-	// TODO: Add more methods for printing shapes here:
+    }
+    // TODO: Add more methods for printing shapes here:
 
     public void printRectangleHollow(int width, int height) {
         System.out.println("Hollow Rectangle:");
@@ -55,27 +56,30 @@ class PrintHelper {
 }
 
 public class PrintingShapes extends Object {
-	public static void main(String[] args) {
-		// TODO: You will also add more code to this main method
-		PrintHelper printer = new PrintHelper();
+    public static void main(String[] args) {
+        // TODO: You will also add more code to this main method
+        PrintHelper printer = new PrintHelper();
         Scanner keyboard = new Scanner(System.in);
 
         System.out.println("Hello! How many stars would you like me to print?");
         if (keyboard.hasNextInt()) {
             printer.printStars(keyboard.nextInt());
-        } else return;
+        } else
+            return;
 
         System.out.println("For a rectangle, how wide should it be?");
         int width;
         int height;
         if (keyboard.hasNextInt()) {
             width = keyboard.nextInt();
-        } else return;
+        } else
+            return;
 
         System.out.println("How high should it be?");
         if (keyboard.hasNextInt()) {
             height = keyboard.nextInt();
-        } else return;
+        } else
+            return;
         printer.printRectangle(width, height);
         width = 0;
         height = 0;
@@ -83,17 +87,20 @@ public class PrintingShapes extends Object {
         System.out.println("For a hollow rectangle, how wide should it be?");
         if (keyboard.hasNextInt()) {
             width = keyboard.nextInt();
-        } else return;
+        } else
+            return;
         System.out.println("How high should it be?");
         if (keyboard.hasNextInt()) {
             height = keyboard.nextInt();
-        } else return;
+        } else
+            return;
         printer.printRectangleHollow(width, height);
 
         System.out.println("For a left triangle, how high should it be?");
         if (keyboard.hasNextInt()) {
             printer.printLeftTriangle(keyboard.nextInt());
-        } else return;
+        } else
+            return;
 
-	}
+    }
 }
